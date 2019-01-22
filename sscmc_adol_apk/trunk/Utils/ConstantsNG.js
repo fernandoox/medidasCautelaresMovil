2ad9731d911d1a5a85099fc5005bab0ed9d93f08
Luis Fernando Fernández Cruz <luis.fernan.fz@gmail.com>
2019-01-22 17:28:23 -0600

    Remove trunk SVN

--- sscmc_adol_apk/trunk/Utils/ConstantsNG.js
+++
@@ -1,9 +0,0 @@
-global.ESTATUS_SOLICITUD = {
-    PENDIENTE       : 1,
-    TURNADO         : 2,
-    RECHAZADO       : 3,
-    ASIGNADO        : 4,
-    EVALUADO        : 5,
-    VALIDADO        : 6,
-    CONCLUIDO       : 7
-}
--- sscmc_adol_apk/trunk/Utils/Database.js
+++
@@ -1,7 +0,0 @@
-import { SQLite } from 'expo';
-
-// QA
-//module.exports = SQLite.openDatabase('evaluacionesSQLite.db');
-
-// PROD
-module.exports = SQLite.openDatabase('evaluacionesSQLiteAPK.db');
